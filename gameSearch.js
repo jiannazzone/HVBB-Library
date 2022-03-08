cceffd31a7370c9f67352509c79695752252f3c7
Adam Iannazzone <46025473+jiannazzone@users.noreply.github.com>
2022-03-08 14:20:30 -0500

    Add selectability to search results

--- gameSearch.js
+++ gameSearch.js
@@ -1,8 +1,6 @@
-// const input = document.getElementById("gameInput");
-// input.addEventListener("input", makeAPIcall);
+var activeGame = "";
 
 function makeAPIcall(val) {
-    document.getElementById("searchResults").innerHTML = "";
     let thisUrl = "https://boardgamegeek.com/xmlapi2/search?type=boardgame,boardgameexpansion&query=" + val;
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
@@ -15,6 +13,15 @@ function makeAPIcall(val) {
 }
 
 function createGameList(xml) {
+
+    // Prepare UI by hiding previous results
+    const gameSearchResults = document.getElementById("gameSearchResults");
+    gameSearchResults.style.visibility = "hidden";
+    gameSearchResults.style.borderStyle = "none";
+
+    // Clear all child divs inside gameSearchResults
+    clearDiv(gameSearchResults);
+
     let gameList = {};
     let x, i, xmlDoc, txt;
     xmlDoc = xml.responseXML;
@@ -22,17 +29,57 @@ function createGameList(xml) {
     for (i = 0; i < x.length; i++) {
         const gameId = Number(x[i].id);
         const gameName = String(x[i].getElementsByTagName("name")[0].getAttribute("value"));
-        gameList[gameName] = gameId;
+        gameList[gameId] = gameName;
     }
 
-    console.log(gameList);
-
-    res = document.getElementById("searchResults");
-    res.innerHTML = "";
-    let list = "";
+    // Create list of games from Board Game Geek API
     const keys = Object.keys(gameList);
-    keys.forEach((key, i) => {
-        list += "<li>" + String(key) + "</li>";
+    keys.forEach((key) => {
+        const node = document.createElement("div");
+        node.className = "resultItem";
+        node.id = String(key); // ID from Board Game Geek
+        node.innerHTML = gameList[key]; // Name of game
+        gameSearchResults.appendChild(node);
     });
-    res.innerHTML = "<ul>" + list + "</ul>";
+
+    // Show the results
+    gameSearchResults.style.visibility = "visible";
+    gameSearchResults.style.borderStyle = "solid";
+
+    // Handle click on search results
+    const filteredGames = document.getElementsByClassName("resultItem");
+    for (game of filteredGames) {
+        game.addEventListener("click", function() { selectGame(this, gameSearchResults) });
+    }
+}
+
+function selectGame(game, resultsDiv) {
+    activeGame = game.innerHTML;
+    document.getElementById("gameInput").value = activeGame;
+    console.log(activeGame)
+
+    // Reset UI
+    clearDiv(resultsDiv);
+    resultsDiv.style.visibility = "hidden";
+    gameList = {};
+}
+
+function clearDiv(thisDiv) {
+    while (thisDiv.firstChild) {
+        thisDiv.removeChild(thisDiv.lastChild);
+    }
+}
+
+function clearSearch(inputId) {
+    thisInput = document.getElementById(inputId);
+    thisInput.value = "";
+
+    // Prepare UI by hiding previous results
+    const gameSearchResults = document.getElementById("gameSearchResults");
+    gameSearchResults.style.visibility = "hidden";
+    gameSearchResults.style.borderStyle = "none";
+
+    // Clear all child divs inside gameSearchResults
+    clearDiv(gameSearchResults);
+
 }
