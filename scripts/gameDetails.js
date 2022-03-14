0c60eca830a37e4037310a11c3cb4a7de0be08bb
Adam Iannazzone <46025473+jiannazzone@users.noreply.github.com>
2022-03-14 15:33:32 -0400

    Add BGG rating

--- scripts/gameDetails.js
+++ scripts/gameDetails.js
@@ -4,7 +4,7 @@ function makeAPIcall() {
     const params = new URLSearchParams(location.search);
     const gameID = params.get('id');
 
-    let thisUrl = "https://boardgamegeek.com/xmlapi2/thing?id=" + gameID;
+    let thisUrl = `https://boardgamegeek.com/xmlapi2/thing?id=${gameID}&stats=1`;
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
@@ -19,9 +19,24 @@ function getGameDetails(xml) {
     const xmlDoc = xml.responseXML;
     const x = xmlDoc.getElementsByTagName('item')[0];
 
-    // Set title
+    // Title
     const gameTitle = document.getElementById('game-title');
     gameTitle.innerHTML = x.getElementsByTagName("name")[0].getAttribute("value");
+
+    // Rating
+    const rating = document.getElementById('rating');
+    const gameRating = Number(x.getElementsByTagName('statistics')[0].getElementsByTagName('ratings')[0].getElementsByTagName('average')[0].getAttribute('value'));
+    rating.innerHTML += ` ${gameRating.toFixed(1)} / 10`;
+    if (gameRating >= 8) {
+        rating.style.backgroundColor = 'darkgreen';
+        rating.style.color = 'white';
+    } else if (gameRating >= 6) {
+        rating.style.backgroundColor = 'gold';
+    } else if (gameRating >= 4) {
+        rating.style.backgroundColor = 'darkorange'
+    } else {
+        rating.style.backgroundColor = 'darkred';
+    }
 
     // Image
     const gameImage = document.getElementById('game-image');
